9b2f93bd0a23a636446b273f5537ba055993dae9
Victor Henri S. Vargas <103096096+imyudz@users.noreply.github.com>
2024-02-04 21:42:07 -0300

    feat: room page finalized with some responsivity things to adjust

--- frontend/src/app/room/[id]/page.tsx
+++ frontend/src/app/room/[id]/page.tsx
@@ -1,11 +1,47 @@
+import Chat from "@/components/Chat";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 export default function Room({params}: {params: {id: string}}){
     return (
-        <>
-            <Header/>   
+        <div className="h-screen">
+            <Header/>
+            <main className="max-w-[86rem] grid lg:grid-cols-[1fr_25rem] grid-cols-[1fr] mx-auto my-2 gap-4 h-[70%]">
+                <div className="grid md:grid-cols-2 grid-cols-1 gap-4 md:grid-rows-2">
+                    <div className="p-2 bg-gray-400 rounded-md w-full h-full relative">
+                        <video className="w-full h-full overflow-hidden"></video>
+                        <span className="absolute bottom-3">Participant Name</span>
+                    </div>
+                    <div className="p-2 bg-gray-400 rounded-md w-full h-full relative">
+                        <video className="w-full h-full overflow-hidden"></video>
+                        <span className="absolute bottom-3">Participant Name</span>
+                    </div>
+                    <div className="p-2 bg-gray-400 rounded-md w-full h-full relative">
+                        <video className="w-full h-full overflow-hidden"></video>
+                        <span className="absolute bottom-3">Participant Name</span>
+                    </div>
+                    <div className="p-2 bg-gray-400 rounded-md w-full h-full relative">
+                        <video className="w-full h-full overflow-hidden"></video>
+                        <span className="absolute bottom-3">Participant Name</span>
+                    </div>
+                    
+                    {/* <div className="p-2 bg-gray-400 rounded-md min-w-full sm:min-w-[calc(50%-1rem)] h-[calc(50%-1rem)] relative">
+                        <video></video>
+                        <span className="absolute bottom-3">Participant Name</span>
+                    </div>
+                    <div className="p-2 bg-gray-400 rounded-md min-w-full sm:min-w-[calc(50%-1rem)] h-[calc(50%-1rem)] relative">
+                        <video></video>
+                        <span className="absolute bottom-3">Participant Name</span>
+                    </div>
+                    <div className="p-2 bg-gray-400 rounded-md min-w-full sm:min-w-[calc(50%-1rem)] h-[calc(50%-1rem)] relative">
+                        <video></video>
+                        <span className="absolute bottom-3">Participant Name</span>
+                    </div> */}
+                    
+                </div>
+                <Chat/>
+            </main>   
             <Footer/>
-        </>
+        </div>
     );
 }
--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,18 +1,30 @@
+import Image from "next/image";
+
 export default function Chat(){
     return (
-        <aside className="h-full bg-gray-400">
-            <div>
-                <span>name</span>
-                <span>12:34</span>
-                <div>
-                    <span>text</span>
+        <aside className=" bg-gray-500 p-4 rounded-md hidden lg:block">
+            <div className="relative h-full">
+                <div className="bg-gray-400 rounded-md p-3 space-y-1">
+                    <div>
+                        <span className="font-medium text-base text-primary-light">name</span>
+                        <span className="float-right text-sm text-primary-light">12:34</span>
+                    </div>
+                    <div className="text-sm">
+                        <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae totam quisquam ullam harum excepturi, magnam, veritatis non tenetur minus doloremque explicabo provident mollitia eum autem quae veniam nesciunt fuga magni.</span>
+                    </div>
                 </div>
+                
+
+                <form action="" className="absolute bottom-2 w-full">
+                    <div className="flex relative">
+                        <input type="text" name="" id="" className="px-3 py-2 outline-none border-transparent border-2 border-solid focus:border-primary-light bg-gray-400 rounded-md w-full  placeholder:text-gray-200"/>
+                        <Image src="/send-arrow.svg" width={25} height={20} alt="enviar comentário" className="absolute cursor-pointer right-2 top-2 bg-gray-400"/>
+                    </div>
+
+
+                </form>
             </div>
-
-            <form action="">
-                <input type="text" name="" id=""/>
-
-            </form>
+            
 
         </aside>
     );
